dd0c667468bed73624f4c78886fde96144f0d523
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-11-10 11:41:45 +0530

    chore: fixed navbar styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,17 +4,22 @@ import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
-    <header className="padding-x py-8">
-      <nav className="flex outline-dashed justify-between max-container items-center">
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between max-container items-center">
         <a>
           <img src={headerLogo} alt="Logo" width={130} height={30}></img>
         </a>
 
-        <ul className="flex justify-between items-center gap-16 ring-2 ring-dodgerblue max-lg:hidden">
+        <ul className="flex justify-between items-center gap-16 max-lg:hidden">
           {navLinks.map((link) => (
-            <li key={link.label}><a href={link.href} className="text-slate-gray text-lg font-montserrat leading-normal">
-              {link.label}
-            </a></li>
+            <li key={link.label}>
+              <a
+                href={link.href}
+                className="text-slate-gray text-lg font-montserrat leading-normal"
+              >
+                {link.label}
+              </a>
+            </li>
           ))}
         </ul>
 
